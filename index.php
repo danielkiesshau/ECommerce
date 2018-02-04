f288eebf0d7c39745aad4956f60ef0bbb96fa145
Daniel Kiesshau <danielkiesshau@outlook.com>
2018-02-04 12:11:07 -0200

    added PageAdmin class

--- index.php
+++ index.php
@@ -1,18 +1,26 @@
 <?php 
 
 require_once("vendor/autoload.php");
+use \Slim\Slim;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
 
-$app = new \Slim\Slim();
+
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    $page = new \Hcode\Page();
+    $page = new Page();
     $page->setTpl("index");
-	$sql = new \Hcode\DB\Sql();
-   
-
 });
+
+$app->get('/admin', function() {
+    $page = new PageAdmin();
+    $page->setTpl("index");
+});
+
 
 $app->run();
 
---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,10 @@
+<?php
+namespace Hcode;
+class PageAdmin extends Page{
+    public function __constructor($opts = array(),$tpl_dir="/views/admin/"){
+        parent::__construct($opts, $tpl_dir);
+    }
+}
+
+
+?>
